0ab7a31a18c900902547d742164b0977ad867048
Filipe Abreu <filabreu@gmail.com>
2022-04-27 11:30:05 -0300

    Parse kills with addKill method in LogParser * Add list of meansOfDeath in Game class * Add map to track kills by player for each Game instance * Add map to track kills by means for each Game instance * When parsing kill in addKill LogParser, add kill count to player if he killed another player * When parsing kill in addKill LogParser, decrese kill count to player if was killed by world * When parsing kill in addKill LogParser, add kill count to kills by means

--- src/parser.js
+++ src/parser.js
@@ -4,7 +4,43 @@ class Game {
   constructor(name) {
     this.name = name;
     this.playerMap = {};
+    this.killsMap = {};
+    this.killsMeansMap = {};
+
+    Game.meansOfDeath.map((_mean, i) => this.killsMeansMap[i] = 0);
   }
+
+  static meansOfDeath = [
+    'MOD_UNKNOWN',
+    'MOD_SHOTGUN',
+    'MOD_GAUNTLET',
+    'MOD_MACHINEGUN',
+    'MOD_GRENADE',
+    'MOD_GRENADE_SPLASH',
+    'MOD_ROCKET',
+    'MOD_ROCKET_SPLASH',
+    'MOD_PLASMA',
+    'MOD_PLASMA_SPLASH',
+    'MOD_RAILGUN',
+    'MOD_LIGHTNING',
+    'MOD_BFG',
+    'MOD_BFG_SPLASH',
+    'MOD_WATER',
+    'MOD_SLIME',
+    'MOD_LAVA',
+    'MOD_CRUSH',
+    'MOD_TELEFRAG',
+    'MOD_FALLING',
+    'MOD_SUICIDE',
+    'MOD_TARGET_LASER',
+    'MOD_TRIGGER_HURT',
+    'MOD_NAIL',
+    'MOD_CHAINGUN',
+    'MOD_PROXIMITY_MINE',
+    'MOD_KAMIKAZE',
+    'MOD_JUICED',
+    'MOD_GRAPPLE'
+  ]
 
   get totalKills() {
     return;
@@ -29,7 +65,7 @@ class LogParser {
       this.data = fs.readFileSync(logfile, "utf8");
     } catch (err) {
       console.error(err);
-    };
+    }
 
     this.games = [];
     this.currentGame;
@@ -45,11 +81,12 @@ class LogParser {
 
   static addPlayer(data, parser) {
     const dataMatch = data.match(/ClientConnect: (\d)/);
-    
+
     if (dataMatch) {
       const playerId = dataMatch[1];
 
-      parser.currentGame.playerMap[playerId] = '';
+      parser.currentGame.playerMap[playerId] = "";
+      parser.currentGame.killsMap[playerId] = 0;
     }
   }
 
@@ -64,11 +101,29 @@ class LogParser {
     }
   }
 
-  static gamePropertyHandlers = {
-    ['InitGame']: this.initGame,
-    ['ClientConnect']: this.addPlayer,
-    ['ClientUserinfoChanged']: this.changePlayer,
+  static addKill(data, parser) {
+    const dataMatch = data.match(/Kill: (\d) (\d) (\d)/);
+
+    if (dataMatch) {
+      const killerId = dataMatch[1];
+      const killedId = dataMatch[2];
+      const deathMeanId = dataMatch[3];
+
+      if (!parser.currentGame.playerMap[killerId]) {
+        parser.currentGame.killsMap[killedId]--;
+      } else if (killerId !== killedId) {
+        parser.currentGame.killsMap[killerId]++;
+        parser.currentGame.killsMeansMap[deathMeanId]++;
+      }
+    }
   }
+
+  static gamePropertyHandlers = {
+    ["InitGame"]: this.initGame,
+    ["ClientConnect"]: this.addPlayer,
+    ["ClientUserinfoChanged"]: this.changePlayer,
+    ["Kill"]: this.addKill,
+  };
 
   static gameParserHandlerMatcher(row, parser) {
     for (const properyMatcher in this.gamePropertyHandlers) {
@@ -83,6 +138,8 @@ class LogParser {
     for (const [i, row] of this.rows.entries()) {
       LogParser.gameParserHandlerMatcher(row, this);
     }
+
+    console.log(this.games);
   }
 
   get rows() {
@@ -92,5 +149,3 @@ class LogParser {
 
 const parser = new LogParser("./logs/qgames.log");
 parser.parse();
-
-console.log(parser.games)
